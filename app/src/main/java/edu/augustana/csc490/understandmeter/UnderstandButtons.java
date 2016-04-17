874e9c3dadbe44b358c964adff6f0a3a25c4b77b
kelseyself14 <kelseyself14@augustana.edu>
2016-04-17 16:11:55 -0500

    Added Button Activity

--- app/src/main/java/edu/augustana/csc490/understandmeter/UnderstandButtons.java
+++ app/src/main/java/edu/augustana/csc490/understandmeter/UnderstandButtons.java
@@ -2,14 +2,12 @@ package edu.augustana.csc490.understandmeter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class UnderstandButtons extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_understand_buttons);
     }
 }
