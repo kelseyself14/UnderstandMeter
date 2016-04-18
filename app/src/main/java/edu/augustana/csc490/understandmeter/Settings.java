a393b6ef9e55f42b8b281bc264ae60bff175fc03
Scott Davis <scottdavis12@augustana.edu>
2016-04-18 15:26:55 -0500

    Settings activity should now launch

--- app/src/main/java/edu/augustana/csc490/understandmeter/Settings.java
+++ app/src/main/java/edu/augustana/csc490/understandmeter/Settings.java
@@ -1,7 +1,19 @@
 package edu.augustana.csc490.understandmeter;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
 /**
  * Created by Scott Dav on 4/18/2016.
  */
-public class Settings {
+public class Settings extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.class_settings);
+        Intent intent = getIntent();
+        int option = intent.getIntExtra("CreateClass", 0);
+
+    }
 }
