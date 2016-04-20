b473aae99116d696a09aa79e45c6ed9306408190
Scott Davis <scottdavis12@augustana.edu>
2016-04-20 15:44:56 -0500

    Added a timer to the student view

--- app/src/main/java/edu/augustana/csc490/understandmeter/UnderstandButtons.java
+++ app/src/main/java/edu/augustana/csc490/understandmeter/UnderstandButtons.java
@@ -1,13 +1,18 @@
 package edu.augustana.csc490.understandmeter;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class UnderstandButtons extends AppCompatActivity {
+
+    private TextView mCountDown;
+    private boolean value = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,24 +20,20 @@ public class UnderstandButtons extends AppCompatActivity {
         setContentView(R.layout.activity_understand_buttons);
         Intent intent = getIntent();
         int option = intent.getIntExtra("JoinClass", 0);
-        Button understand = (Button)  findViewById(R.id.IUnderstand);
-        understand.setOnClickListener(displayUnderstand);
         Button NotUnderstand = (Button)  findViewById(R.id.NotUnderstand);
         NotUnderstand.setOnClickListener(displayNotUnderstand);
     }
-    private View.OnClickListener displayUnderstand = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            // MAKE A METHOD CALL TO DISPLAY THE INFORMATION
-            displayToast("Understood");
-
-        }
-    };
     private View.OnClickListener displayNotUnderstand = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             // MAKE A METHOD CALL TO DISPLAY THE INFORMATION
-            displayToast("Not Understood");
+            if(value == false) {
+                value = true;
+                displayToast("Not Understood");
+                startCountdown();
+            } else {
+                displayToast("Wait for countdown to finish");
+            }
 
         }
     };
@@ -40,5 +41,19 @@ public class UnderstandButtons extends AppCompatActivity {
         // SHOW THE INFORMATION ABOUT THE PAINTING AS
         // A TOAST WITH A SHORT DISPLAY LIFE
         Toast.makeText(this, paintingDescription, Toast.LENGTH_SHORT).show();
+    }
+
+    public void startCountdown(){
+        new CountDownTimer(10000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+               mCountDown.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                mCountDown.setText("0");
+            }
+        }.start();
+        value = false;
     }
 }
