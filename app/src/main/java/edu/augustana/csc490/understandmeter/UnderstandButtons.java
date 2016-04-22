0d55ae5d5ad1e8bb34c9f54794bbea38265fd119
scottdavis12 <scottdavis12@augustana.edu>
2016-04-22 15:07:28 -0500

    Added locations for firebase code to be implemented

--- app/src/main/java/edu/augustana/csc490/understandmeter/UnderstandButtons.java
+++ app/src/main/java/edu/augustana/csc490/understandmeter/UnderstandButtons.java
@@ -12,7 +12,8 @@ import android.widget.Toast;
 public class UnderstandButtons extends AppCompatActivity {
 
     private TextView mCountDown;
-    private boolean value = false;
+    private boolean value = true;
+    private Button notUnderstand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +21,8 @@ public class UnderstandButtons extends AppCompatActivity {
         setContentView(R.layout.activity_understand_buttons);
         Intent intent = getIntent();
         int option = intent.getIntExtra("JoinClass", 0);
-        Button NotUnderstand = (Button)  findViewById(R.id.NotUnderstand);
-        NotUnderstand.setOnClickListener(displayNotUnderstand);
+        notUnderstand = (Button)  findViewById(R.id.NotUnderstand);
+        notUnderstand.setOnClickListener(displayNotUnderstand);
         mCountDown=(TextView) findViewById(R.id.mCountDown);
 
     }
@@ -29,14 +30,14 @@ public class UnderstandButtons extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             // MAKE A METHOD CALL TO DISPLAY THE INFORMATION
-            if(value == false) {
-                value = true;
-                displayToast("Not Understood");
-                startCountdown();
-            } else {
-                displayToast("Wait for countdown to finish");
-            }
+            value = false;
+            notUnderstand.setEnabled(value);
+            notUnderstand.setClickable(value);
+            displayToast("You Don't Understand! Teacher Notified");
 
+            // Add code for send info via fire base!!!
+
+            startCountdown();
         }
     };
     private void displayToast(String description) {
@@ -54,6 +55,10 @@ public class UnderstandButtons extends AppCompatActivity {
                 mCountDown.setText("Press Again");
             }
         }.start();
-        value = false;
+        value = true;
+        notUnderstand.setEnabled(value);
+        notUnderstand.setClickable(value);
+
+        // Add code for sending a "negative tick" via fire base!!!
     }
 }
