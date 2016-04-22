4933d624bfa383081091b96cb6c0616de5680fd7
kelseyself14 <kelseyself14@augustana.edu>
2016-04-22 15:46:55 -0500

    added teacher counter

--- app/src/main/java/edu/augustana/csc490/understandmeter/Teacher_View.java
+++ app/src/main/java/edu/augustana/csc490/understandmeter/Teacher_View.java
@@ -1,5 +1,6 @@
 package edu.augustana.csc490.understandmeter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class Teacher_View extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher__view);
+        Intent intent = getIntent();
+        int option = intent.getIntExtra("CreateClass", 0);
     }
 }
