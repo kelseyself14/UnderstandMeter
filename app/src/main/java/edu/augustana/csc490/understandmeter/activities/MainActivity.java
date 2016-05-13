c2c8e8035f42ce7625a256644e67b99babbf4b48
Scott Davis <scottdavis12@augustana.edu>
2016-05-13 14:10:04 -0500

    Added comments for the main activity class

--- app/src/main/java/edu/augustana/csc490/understandmeter/activities/MainActivity.java
+++ app/src/main/java/edu/augustana/csc490/understandmeter/activities/MainActivity.java
@@ -8,14 +8,18 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import edu.augustana.csc490.understandmeter.R;
-
+/*
+This Class runs the activity which is the start up screen. The
+Start up screen displays the name of the app and then prompts the
+user to either create a class or join a class.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+// Establishing the Create Class button and click listener for this button
         Button createClass = (Button) findViewById(R.id.createClass);
         createClass.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Settings.class));
             }
         });
-
+// Establishing the Join Class button and click listener for this button
         Button joinClass = (Button)  findViewById(R.id.joinClass);
         joinClass.setOnClickListener(new View.OnClickListener() {
             @Override
